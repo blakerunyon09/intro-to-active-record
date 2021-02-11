b46c8d6bc338b1645ec699a77a5d29bf6c540b80
angeloxenakis <angeloxenakis@gmail.com>
2021-02-11 12:49:27 -0600

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+
+require 'bundler/setup'
+Bundler.require
+
+# Establish a database connection here
+
+require_relative "../dog.rb"
